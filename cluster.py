3be4eb86f17b0cba98b1cdc37fb71ff7d695844d
drafterleo <drafterleo@gmail.com>
2017-06-24 18:50:25 +0300

    watch point and cluster stats

--- cluster.py
+++ cluster.py
@@ -1,13 +1,25 @@
+from bitarray import bitarray
 
 
 class Cluster:
     def __init__(self,
                  bits: tuple,
+                 bit_mask: bitarray,
                  activate_threshold: int):
         self.bits = bits
         self.bit_set = set(bits)
+        self.bit_mask = bit_mask
         self.activate_threshold = activate_threshold
+        self.stats = dict()
 
-    def activate(self, active_bits: set):
-        pass
+    def activate(self, bits: set):
+        key = tuple(sorted(bits))
+        self.stats[key] = self.stats.get(key, 0) + 1
+
+    def stat_parts(self) -> dict:
+        parts = {}
+        total_activations = sum(self.stats.values())
+        for key, activations in self.stats.items():
+            parts[key] = activations / total_activations
+        return parts
 
--- watch_point.py
+++ watch_point.py
@@ -1,4 +1,5 @@
 from cluster import Cluster
+from bitarray import bitarray
 
 
 class WatchPoint:
@@ -15,22 +16,30 @@ class WatchPoint:
         self.clusters = dict()
 
     def process_input(self, input_bits: set):
-        active_bits = set.intersection(input_bits, self.watch_bit_set)
-        if len(active_bits) >= self.cluster_make_threshold:
-            self.add_cluster(tuple(sorted(active_bits)))
+        active_bits = input_bits & self.watch_bit_set
         if len(active_bits) >= self.cluster_activate_threshold:
-            self.update_clusters(active_bits)
+            bits = [1 if bit_idx in active_bits else 0
+                    for bit_idx in self.watch_bits]
+            bit_mask = bitarray(bits)
+            if len(active_bits) >= self.cluster_make_threshold:
+                self.add_cluster(tuple(sorted(active_bits)), bit_mask)
+            self.update_clusters(active_bits, bit_mask)
 
-    def add_cluster(self, bit_key: tuple):
+    def add_cluster(self, bit_key: tuple, bit_mask: bitarray):
         if bit_key not in self.clusters:
             self.clusters[bit_key] = Cluster(bits=bit_key,
+                                             bit_mask=bit_mask,
                                              activate_threshold=self.cluster_activate_threshold)
 
-    def update_clusters(self, active_bits: set):
+    def update_clusters(self, bits: set, bit_mask: bitarray):
         for cluster in self.clusters.values():
-            intersection = active_bits & cluster.bit_set
+            intersection = bits & cluster.bit_set
             if len(intersection) >= self.cluster_activate_threshold:
                 cluster.activate(intersection)
+        # for cluster in self.clusters.values():
+        #     intersection = bit_mask & cluster.bit_mask
+        #     if intersection.count(1) >= self.cluster_activate_threshold:
+        #         cluster.activate(intersection)
 
 
 
